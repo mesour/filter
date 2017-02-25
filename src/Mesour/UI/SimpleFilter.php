56b981fbcc5d86559fa1bee372ba8871c61790a5
mesour <matous.nemec@gmail.com>
2017-02-25 15:27:20 +0100

    fixed test, minor changes

--- src/Mesour/UI/SimpleFilter.php
+++ src/Mesour/UI/SimpleFilter.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Mesour Filter (http://components.mesour.com/component/filter)
  *
- * Copyright (c) 2015-2016 Matouš Němec (http://mesour.com)
+ * Copyright (c) 2017 Matouš Němec (http://mesour.com)
  *
  * For full licence and copyright please view the file licence.md in root of this project
  */
